fccfecaf379a923f695c10da0b502326c69b52ce
HugoRoca <hugo.rock20@hotmail.com>
2019-03-29 10:05:03 -0500

    move files in folder v1

--- v1/app.js
+++ v1/app.js
@@ -2,8 +2,9 @@ const config = require("./config");
 const AWS = require("aws-sdk");
 
 let kinesis = new AWS.Kinesis({
-    apiVersion: "2013-12-02",
-    region: "us-east-1"
+    //apiVersion: "2013-12-02",
+    region: "us-east-1",
+    params: { StreamName: config.kinesis.streamName }
 });
 /*
 class Base {
@@ -32,6 +33,35 @@ class Base {
 */
 (async () => {
     try {
+
+        // see below for options
+        var readable = require('kinesis-readable')(kinesis);
+
+        readable
+            // 'data' events will trigger for a set of records in the stream
+            .on('data', function (records) {
+                //console.log(records);
+                for (const key in records) {
+                    const element = records[key];
+                    let payload = new Buffer(element.Data, 'base64').toString('ascii');
+                    console.log('records.Data:', payload);
+                }
+            })
+            // each time a records are passed downstream, the 'checkpoint' event will provide
+            // the last sequence number that has been read
+            .on('checkpoint', function (sequenceNumber) {
+                console.log(sequenceNumber);
+            })
+            .on('error', function (err) {
+                console.error(err);
+            })
+            .on('end', function () {
+                console.log('all done!');
+            });
+
+        /*setTimeout(function () {
+            readable.close();
+        }, 2000);*/
         /*kinesis.putRecord({
             Data: '{"action": "click", "productId": "product-123", "Probando": "ando"}',
             PartitionKey: config.kinesis.partitionKey,
@@ -43,7 +73,7 @@ class Base {
                 console.log(data); // successful response
             }
         });*/
-        kinesis.describeStream({
+        /*kinesis.describeStream({
             StreamName: config.kinesis.streamName
         }, (err, streamData) => {
             if (err) console.log(err, err.stack);
@@ -55,16 +85,17 @@ class Base {
 
                     kinesis.getShardIterator({
                         ShardId: shard.ShardId,
-                        ShardIteratorType: "AT_SEQUENCE_NUMBER",
-                        StreamName: config.kinesis.streamName,
-                        StartingSequenceNumber: "49593293221786655550236802004103412906377425557158625282"
+                        ShardIteratorType: "TRIM_HORIZON",
+                        StreamName: config.kinesis.streamName
                     }, (err, shardIteratorData) => {
                         if (err) console.log(err, err.stack);
                         else {
                             console.log("**** shardIteratorData", shardIteratorData);
+                            console.log("**** shardIteratorData", shardIteratorData);
 
                             kinesis.getRecords({
-                                ShardIterator: shardIteratorData.ShardIterator
+                                ShardIterator: shardIteratorData.ShardIterator,
+                                Limit: 1000
                             }, (err, recordsData) => {
                                 if (err) console.log(err, err.stack);
                                 else {
@@ -76,7 +107,7 @@ class Base {
 
                 });
             }
-        });
+        });*/
     } catch (error) {
         console.error(error);
     }
